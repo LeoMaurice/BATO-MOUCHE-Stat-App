bb1a2c755dd80471f587e1f22f6a67718e3e03fb
mthaury <marie-olive.thaury@ensae.fr>
2023-05-04 16:40:45 +0000

    opacity

--- helpers/visualize.py
+++ helpers/visualize.py
@@ -1,19 +1,39 @@
 import folium
 import pandas as pd
 import geopandas as gpd
+from matplotlib import colormaps
+from matplotlib.colors import ListedColormap
+
 
 def folium_grid_cat_plot(gdf, var : str, cmap = 'Set1', 
-coordinates =(48.8534100,2.3488000),zoom_start=12.1):
-    m = folium.Map(coordinates, zoom_start = zoom_start)
-    m = gdf.explore(
-        m = m,
-        column = var,
-        tooltip = var,
-        tiles = 'OpenStreetMap',
-        popup = True,
-        cmap = cmap,
-        style_kwds = dict(color = "black", opacity = 0.6,
-        fillOpacity = 0.4)
-    )
+coordinates =(48.8534100,2.3488000),zoom_start=12.1, discrete = False, op = 0.6):
+    if discrete:
+        colors = colormaps[cmap](range(len(gdf[var].unique())))
+        colors = ListedColormap(colors)
+        m = folium.Map(coordinates, zoom_start = zoom_start)
+        m = gdf.explore(
+            m = m,
+            column = var,
+            tooltip = var,
+            tiles = 'OpenStreetMap',
+            popup = True,
+            cmap = colors,
+            categorical = True,
+            style_kwds = dict(color = "black", opacity = op,
+            fillOpacity = 0.4)
+        )
+    else:
+        m = folium.Map(coordinates, zoom_start = zoom_start)
+        m = gdf.explore(
+            m = m,
+            column = var,
+            tooltip = var,
+            tiles = 'OpenStreetMap',
+            popup = True,
+            cmap = cmap,
+            style_kwds = dict(color = "black", opacity = op,
+            fillOpacity = 0.4)
+        )
+
 
     return m
